2d25554977f2049d42b2063e446d6648e0bf69f5
akashdanda <79346459+akashdanda@users.noreply.github.com>
2024-01-08 20:19:08 -0800

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -70,7 +70,9 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
 
   @Override
   public void teleopInit() {
